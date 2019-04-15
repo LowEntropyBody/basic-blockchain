8ccdf7434e7e029c99cd9a8138c34d91746285ae
yazhao <yazhao@microsoft.com>
2019-04-15 15:11:01 +0800

    blot

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -2,21 +2,70 @@ package blockchain
 
 import (
 	. "github.com/LowEntropyBody/basic-blockchain/block"
+	"github.com/boltdb/bolt"
 )
 
 // Blockchain struct
 type Blockchain struct {
-	Blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
+
+var dbFile = "blockchain.db"
+var blocksBucket = "blocksBucket"
 
 // NewBlockchain - construct
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	db, _ := bolt.Open(dbFile, 0600, nil)
+
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, _ := tx.CreateBucket([]byte(blocksBucket))
+			b.Put(genesis.Hash, genesis.Serialize())
+			b.Put([]byte("l"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	bc := Blockchain{tip, db}
+
+	return &bc
 }
 
 // AddBlock - add block to chain
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	var lastHash []byte
+
+	bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
+	newBlock := NewBlock(data, lastHash)
+
+	bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		b.Put(newBlock.Hash, newBlock.Serialize())
+		b.Put([]byte("l"), newBlock.Hash)
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
+}
+
+// Iterator -
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+
+	return bci
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/LowEntropyBody/basic-blockchain
 
 go 1.12
+
+require github.com/boltdb/bolt v1.3.1
